23196a0dc2053431215749ac84a7c8bea10b326f
treblot <treblotnad@gmail.com>
2024-02-14 19:04:25 -0600

    some more starter code

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { User, Post, Comment } = require("../models");
+const { User } = require("../models");
 const withAuth = require("../utils/auth");
 const { QueryTypes } = require("sequelize");
 
@@ -32,14 +32,14 @@ router.get("/", async (req, res) => {
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/dashboard");
+    res.redirect("/account");
     return;
   }
 
   res.render("login");
 });
 
-router.get("/dashboard", withAuth, async (req, res) => {
+router.get("/acount", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -48,39 +48,9 @@ router.get("/dashboard", withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-    res.render("dashboard", {
+    res.render("acount", {
       ...user,
       logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/post/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ["user_name"],
-        },
-        {
-          model: Comment,
-          include: [{ model: User, attributes: ["user_name"] }],
-        },
-      ],
-    });
-    const post = postData.get({ plain: true });
-    const commentUserArray = [];
-    for (let i = 0; i < post.comments.length; i++) {
-      commentUserArray.push(post.comments[i].user.user_name);
-      post.comments[i].user = commentUserArray[i];
-    }
-
-    res.render("post", {
-      ...post,
-      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
---
+++ models/card.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Card extends Model {}
+
+Card.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: tru,
+      allowNull: false,
+    },
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+module.exports = User;
