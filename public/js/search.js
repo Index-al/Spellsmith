07479ae73bd4739f8a46fdb5c8319d016554f85c
Indexal <148807519+Index-al@users.noreply.github.com>
2024-02-21 19:04:20 -0600

    pause

--- public/js/search.js
+++ public/js/search.js
@@ -2,10 +2,14 @@ async function searchHandler(e) {
   e.preventDefault();
   let searchText = document.querySelector(".search-input").value.trim();
 
-  if (searchText) {
-    searchText = encodeURIComponent(searchText);
-    document.location.replace(`/search-result/${searchText}`);
-  }
+  //TODO: FIX THIS
+  // Replace spaces with dashes
+  searchText = searchText.replace('/\s+/g', '-');
+
+  // Encode URI components other than spaces
+  searchText = encodeURI(searchText);
+
+  document.location.replace(`/search-result/${searchText}`);
 }
 
 const searchForms = document.querySelectorAll(".search-form");
