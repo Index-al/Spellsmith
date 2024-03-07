11da84d8fe1e53b2db18c4e05482d4d0850208fa
Indexal <148807519+Index-al@users.noreply.github.com>
2024-03-07 01:04:45 -0600

    update to fix render(?)

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: "postgres",
+		protocol: "postgres",
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false, // Note: This is not ideal for production, as it disables SSL certificate verification.
+			},
+		},
+	});
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306,
+		}
+	);
 }
 
 module.exports = sequelize;
